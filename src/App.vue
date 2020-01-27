5d7fc4656094b2205cfc2320aabc48a2d455e3d8
Alena Zaharova <sonshiii@yandex.ru>
2020-01-27 22:23:23 +0500

    css

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,104 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-content class="ma-5 pa-1">
+    <div class="app-wrapper">
+    <h3 class="mb-5">Клиенты этой группы</h3>
+    <v-data-table 
+      :headers="headers"
+      :items="peoples"
+      hide-default-footer="false"
+      class="mb-5" >
+      <template v-slot:item.action="{ item }">
+        <v-icon
+          small
+          @click="deleteItem(item)"
+        >
+          mdi-close
+        </v-icon>
+      </template>
+    </v-data-table>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <div class="d-flex justify-space-between align-center w-100 mb-5">
+        <v-select
+          :items="items"
+          label="Выбрать из списка"
+          outlined
+          class="mb--30 mr-4"
+        ></v-select>
+        <v-btn class="button__custom-height mr-4" outlined color="grey" >Добавить</v-btn>
+        <span class="mr-4"> или </span>
+        <a class="link" text small>Создать нового клиента</a>
+    </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <p>Для того, чтобы отредактировать или удалить клиентов из списка, перейдите в раздел <a class="link">Белые списки</a></p>
+    
+  </div>
+  </v-content> 
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  data: () =>({
+        headers: [
+          {
+            text: 'ФИО',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Номер телефона', value: 'number' },
+          { text: 'Группа', value: 'group' },
+          { text: '', value: 'action', sortable: false }
+        ],
+        peoples: [
+          {
+            name: 'Константинов Константин Константинович',
+            number: "+7 (912) 632-32-32",
+            group: "ГАЗПРОМ"
+          },
+          {
+            name: 'Иванов Александр Константинович',
+            number: "+7 (912) 632-32-32",
+            group: "ГАЗПРОМ"
+          }
+        ],
+        items:[""]
+      
+    }),
+    methods:{
+      deleteItem (item) {
+        const index = this.peoples.indexOf(item)
+        confirm('Вы уверены что хотите удалить элемент?') && this.peoples.splice(index, 1)
+      }
+    }
 };
 </script>
+
+
+<style>
+    /*  default style is sooo hard 
+        that why i use !important
+        */
+  .app-wrapper {
+    max-width: 800px;
+  }
+  .w-100 {
+    width: 100%;
+  }
+  .mb--30 {
+    margin-bottom: -30px !important;
+  }
+  .button__custom-height {
+    height: 56px !important;
+  }
+  .link {
+    border-bottom: 1px dashed #000;
+    transition: all 0.3s ease-in;
+    color: #000000 !important;
+  }
+  .link:hover {
+    color: #4A148C !important;
+  }
+</style>
